093bcb70ca8d0b82411099195e87d7e9b14a2553
jenu <jenu.s@edstem.com>
2023-01-12 20:19:45 +0530

    addressbook updated

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -2,36 +2,44 @@
 
 List<Contact> cachedContacts = new List<Contact>();
 
-for (int i = 0; i < 5; i++)
+try
 {
-    string firstName = string.Empty;
-    string lastName = string.Empty;
-    string email = string.Empty;
-    string phone = string.Empty;
+    for (int i = 0; i < 5; i++)
+    {
+        string firstName = string.Empty;
+        string lastName = string.Empty;
+        string email = string.Empty;
+        string phone = string.Empty;
 
-    Console.Write("Enter first name: ");
-    firstName = Console.ReadLine();
-    Console.Write("Enter Last name: ");
-    lastName = Console.ReadLine();
-    Console.Write("Enter phone: ");
-    phone = Console.ReadLine();
-    Console.Write("Enter email: ");
-    email = Console.ReadLine();
-    var contact = new Contact(firstName,lastName, phone, email);
-    addressBook.AddContact(contact);
-    cachedContacts.Add(contact);
+        Console.Write("Enter first name: ");
+        firstName = Console.ReadLine();
+        Console.Write("Enter Last name: ");
+        lastName = Console.ReadLine();
+        Console.Write("Enter phone: ");
+        phone = Console.ReadLine();
+        Console.Write("Enter email: ");
+        email = Console.ReadLine();
+        var contact = new Contact(firstName, lastName, phone, email);
+        addressBook.AddContact(contact);
+        cachedContacts.Add(contact);
+    }
+
+    Console.WriteLine("Printing contacts");
+    addressBook.PrintContacts();
+
+    Console.WriteLine("Sorting contacts");
+    addressBook.SortContacts();
+    addressBook.PrintContacts();
+
+    for (int i = 0; 1 < 5; i++)
+    {
+        Console.WriteLine("Remove contact :" + cachedContacts[i]);
+        addressBook.RemoveContact(cachedContacts[i]);
+        addressBook.PrintContacts();
+    }
 }
 
-Console.WriteLine("Printing contacts");
-addressBook.PrintContacts();
-
-Console.WriteLine("Sorting contacts");
-addressBook.SortContacts();
-addressBook.PrintContacts();
-
-for (int i = 0; 1< 5; i++)
+catch (Exception ex)
 {
-    Console.WriteLine("Remove contact :" + cachedContacts[i]);
-    addressBook.RemoveContact(cachedContacts[i]);
-    addressBook.PrintContacts();
+    Console.WriteLine($"Something went wrong{ex.Message}");
 }
